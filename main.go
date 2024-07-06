e926888a283798b40a1e1cd09c45a31e2d9e48d9
MungaSoftwiz <mwachilumobm@gmail.com>
2024-07-07 02:06:43 +0300

    chore: set up main function

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/MungaSoftwiz/org-authenticator-api/cmd/api"
+)
+
+func main() {
+	server := api.NewAPIServer(":8080", nil)
+	if err := server.Run(); err != nil {
+	log.Fatal(err)
+}
+}
+
