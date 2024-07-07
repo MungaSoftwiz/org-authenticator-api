388d623e016405d6ea6efb683d6652127541a9a1
MungaSoftwiz <mwachilumobm@gmail.com>
2024-07-07 15:10:08 +0300

    refactor: initialise and start the db connection pool

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/MungaSoftwiz/org-authenticator-api/cmd/api"
+	"github.com/MungaSoftwiz/org-authenticator-api/config"
+	"github.com/MungaSoftwiz/org-authenticator-api/db"
 )
 
-func main() {
-	server := api.NewAPIServer(":8080", nil)
-	if err := server.Run(); err != nil {
-	log.Fatal(err)
-}
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Database connected successfully!")
 }
 
+func main() {
+
+	db, err := db.NewPostgreSQLStorage(config.PostgreSQLConfig{
+		Host:     config.Env.Host,
+		User:     config.Env.User,
+		Password: config.Env.Password,
+		Port:     config.Env.Port,
+		DBName:   config.Env.DBName,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
