58e1cd6ed39131ae4325a220fab0a40a70de2395
MungaSoftwiz <mwachilumobm@gmail.com>
2024-07-09 10:28:26 +0300

    feat: add user storage and methods associated

---
+++ service/user/storage.go
@@ -0,0 +1,41 @@
+package user
+
+import (
+	"github.com/MungaSoftwiz/org-authenticator-api/types"
+	"github.com/jmoiron/sqlx"
+)
+
+type Storage struct {
+	db *sqlx.DB
+}
+
+func NewStorage(db *sqlx.DB) *Storage {
+	return &Storage{db: db}
+}
+
+func (s *Storage) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (firstName, lastName, email, password, phone) VALUES (?, ?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password, user.Phone)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Storage) GetUserByEmail(email string) (*types.User, error) {
+	// register user
+	u := new(types.User)
+	err := s.db.Get(u, "SELECT * FROM users WHERE email = $1", email)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+func (s *Storage) GetUserByID(id int) (*types.User, error) {
+	u := new(types.User)
+	err := s.db.Get(u, "SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
